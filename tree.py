38555f7f90764a6a9c3a146751d46f541a0e16a4
belwalter <belwalterv@gmail.com>
2026-08-24 19:47:12 -0300

    delete tree

--- tree.py
+++ tree.py
@@ -1,5 +1,5 @@
 
-from typing import Any
+from typing import Any, Optional
 
 class Node():
 
@@ -31,6 +31,53 @@ class BinaryTree():
             
         self.root = __insert_node(self.root, value)
 
+    def delete_node(self, value: Any) -> Optional[Any]:
+        def __replace(root):
+            print(root.value)
+            aux = None
+            if root.right is None:
+                print('mayor encontrado')
+                return root.left, root
+            else:
+                print('segui buscando a la derecha')
+                root.right, aux = __replace(root.right)
+            return root, aux
+
+        def __delete_node(root, value):
+            x = None
+            if root is not None:
+                if value < root.value:
+                    print('ir a la izq')
+                    input()
+                    root.left, x = __delete_node(root.left,value)
+                elif value > root.value:
+                    print('ir a la derecha')
+                    input()
+                    root.right, x = __delete_node(root.right, value)
+                else:
+                    print('valor encontrado')
+                    input()
+                    x = root.value
+                    aux = None
+                    if root.left is None:
+                        print('no tiene hijo izquierdo')
+                        input()
+                        return root.right
+                    elif root.right is None:
+                        print('no tiene hijo derecho')
+                        input()
+                        return root.left
+                    else:
+                        print('buscar remplazo')
+                        input()
+                        root.left, aux = __replace(root.left)
+                        root.value = aux.value
+            return root, x
+
+        x = None
+        self.root, x = __delete_node(self.root, value)
+
+        return x
 
     def inorden(self) -> None:
         
@@ -60,14 +107,21 @@ class BinaryTree():
 
 arbol = BinaryTree()
 
+arbol.insert_node('F')
+arbol.insert_node('B')
+arbol.insert_node('K')
+arbol.insert_node('E')
 arbol.insert_node('H')
-arbol.insert_node('M')
-arbol.insert_node('D')
-arbol.insert_node('L')
-arbol.insert_node('A')
-arbol.insert_node('Z')
+arbol.insert_node('R')
+arbol.insert_node('G')
+
 
 
 # print(arbol.root.right.left.value)
 
-arbol.postorden()
+arbol.inorden()
+
+print()
+print('eliminar', arbol.delete_node('F'))
+print()
+arbol.inorden()
